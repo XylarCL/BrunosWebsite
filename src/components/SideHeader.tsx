670216d8816b3e328b8c53a2614c7ff0c3e8ea00
XylarCL <christianlogannew@gmail.com>
2025-01-16 15:03:25 +0000

    Changed file structure and img file paths to allow for build

--- src/components/SideHeader.tsx
+++ src/components/SideHeader.tsx
@@ -3,10 +3,10 @@ export default function SideHeader() {
 
     return (
         <div className="sideHeader">
-            <a href="https://www.linkedin.com/in/bruno-debattista-706bb0318/" target="_blank"><img className="sideHeader-content" src="src\assets\imgs\sideHeaderIcons\linkedIn.png" height="50px" width="50px"/></a>
-            <a href="src\imgs\Bruno_Debattista_CV 2.pdf" target="_blank"><img className="sideHeader-content" src="src\assets\imgs\sideHeaderIcons\CV.png" height="50px" width="50px"/></a>
+            <a href="https://www.linkedin.com/in/bruno-debattista-706bb0318/" target="_blank"><img className="sideHeader-content" src="%PUBLIC_URL%\assets\imgs\sideHeaderIcons\linkedIn.png" height="50px" width="50px"/></a>
+            <a href="%PUBLIC_URL%\imgs\Bruno_Debattista_CV 2.pdf" target="_blank"><img className="sideHeader-content" src="%PUBLIC_URL%\assets\imgs\sideHeaderIcons\CV.png" height="50px" width="50px"/></a>
             <a className="email" href="mailto:brudebattista@gmail.com" target="_blank" data-title="brudebattista@gmail.com">
-                <img className="sideHeader-content" src="src\assets\imgs\sideHeaderIcons\email.png" height="50px" width="50px"/> 
+                <img className="sideHeader-content" src="%PUBLIC_URL%\assets\imgs\sideHeaderIcons\email.png" height="50px" width="50px"/> 
                 {/*
                 <div className="slideRight">
                     <h3 className="slideRight-text">brudebattista@gmail.com</h3>
