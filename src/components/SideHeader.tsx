f42edde8998db7751548b4cf26be06d12aa3430c
XylarCL <christianlogannew@gmail.com>
2025-01-18 13:06:13 +0000

    Added working email animation for contact button

--- src/components/SideHeader.tsx
+++ src/components/SideHeader.tsx
@@ -3,14 +3,11 @@ export default function SideHeader() {
 
     return (
         <div className="sideHeader">
-            <a href="https://www.linkedin.com/in/bruno-debattista-706bb0318/" target="_blank"><img className="sideHeader-content" src="\assets\imgs\sideHeaderIcons\linkedIn.png" height="50px" width="50px"/></a>
-            <a href="\imgs\Bruno_Debattista_CV 2.pdf" target="_blank"><img className="sideHeader-content" src="\assets\imgs\sideHeaderIcons\CV.png" height="50px" width="50px"/></a>
-            <a className="email" href="mailto:brudebattista@gmail.com" target="_blank" data-title="brudebattista@gmail.com">
-                <img className="sideHeader-content" src="=\assets\imgs\sideHeaderIcons\email.png" height="50px" width="50px"/> 
-                {/*
-                <div className="slideRight">w
-                    <h3 className="slideRight-text">brudebattista@gmail.com</h3>
-                </div> */}
+            <a className="sideHeader-content" href="https://www.linkedin.com/in/bruno-debattista-706bb0318/" target="_blank"><img className="sideHeader-img" src="\assets\imgs\sideHeaderIcons\linkedIn.png" height="50px" width="50px"/></a>
+            <a className="sideHeader-content" href="\imgs\Bruno_Debattista_CV 2.pdf" target="_blank"><img className="sideHeader-img" src="\assets\imgs\sideHeaderIcons\CV.png" height="50px" width="50px"/></a>
+            <a className="sideHeader-content email" href="mailto:brudebattista@gmail.com" target="_blank" data-title="brudebattista@gmail.com">
+                <img className="sideHeader-img" src="\assets\imgs\sideHeaderIcons\email.png" height="50px" width="50px"/> 
+                <span>brudebattista@gmail.com</span>
             </a>
             <hr className="sideHeader-VR"/>
         </div>
