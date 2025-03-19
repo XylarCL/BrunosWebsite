8dd00d3d4f05b4da6dbf521f2f69f1b4228b6d8e
XylarCL <christianlogannew@gmail.com>
2025-03-19 14:30:50 +0000

    Added example projects and removed old ones.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,9 @@
 import ProjectTile from '../components/ProjectTile';;
 
 export default function Projects() {
-  const projectTileImgs: string[] = ["hello.jpeg", "random.jpeg", "penguins.png", "dolphin.png", "chickens.jpeg"];
-  const projectTileTitles: string[] = ["Pea man", "Josh autobot", "Penguins can fly", "Dolphins: Real or Trumpian Controversy?", "Chickens, just weird birds?"];
-  const projectTitleInfos: string[] = ["This is a highly sophisticated piece of art...", "This is peak human performance...", "Penguins are notorious for a plethora of amazing behaviours...", "Dolphins, thought to be real...", "Chicken Little has been on the run for 50 years..."];
+  const projectTileImgs: string[] = ["ProjectOne.jpg", "ProjectTwo.jpg", "ProjectThree.jpg", "ProjectFour.jpg", "ProjectFive.jpg"];
+  const projectTileTitles: string[] = ["Example One", "Example Two", "Example Three", "Example Four", "Example Five"];
+  const projectTitleInfos: string[] = ["This was a codack flash circuit. After stripping uneeded hardware...", "Using an electromagnetic fault attack, I...", "To start this project I used a bread...", "This project required hooking up to a the debug port on the smart meter...", "I used the single wire debug, with a different microcontroller called black magic...."];
   return (
   <div className='projectTile-container'>
     {projectTileImgs.map((img, index) => (
