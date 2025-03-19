188f5c7ddab633d686d62385564db817aa4995ac
XylarCL <christianlogannew@gmail.com>
2025-03-19 14:12:00 +0000

    Changed paras

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,12 +7,24 @@ export default function About() {
             <div className="aboutPage-content">
                 <h1 className="aboutPage-title">ABOUT ME</h1>
 
-                <h2 className="aboutPage-subtitle">Para 1</h2>
-                <p className="aboutPage-para">Lorem ipsum odor amet, consectetuer adipiscing elit. Adolor consequat litora dapibus inceptos sem. Cras curabitur tempor penatibus vulputate sociosqu nulla. Tempor at vivamus tortor sit ornare ac adipiscing. Penatibus sem ultricies primis convallis libero facilisis nulla dapibus molestie. Tincidunt dolor nunc lectus per ultricies, est sit. Atempor enim auctor adipiscing feugiat lectus accumsan in. Urna dui elit tellus libero rutrum quis. Convallis finibus metus diam at vehicula, lacus litora.</p>
-                <h2 className="aboutPage-subtitle">Para 2</h2>
-                <p className="aboutPage-para">Convallis condimentum leo congue nunc, phasellus at magnis. Facilisi in dignissim ullamcorper urna inceptos bibendum? Dui semper hac quam gravida nisi luctus non. Gravida risus dolor semper euismod consequat pulvinar vivamus dui. Auctor mus proin inceptos; habitasse etiam a. Eu fames conubia sem enim auctor sed etiam! Quisque sagittis purus neque pharetra natoque aenean natoque.</p>
-                <h2 className="aboutPage-subtitle">Para 3</h2>
-                <p className="aboutPage-para">Ultrices tempor semper tempor condimentum pulvinar massa a. Sed eu proin euismod rhoncus tortor tempus libero libero. Sem tristique facilisi quisque in porttitor vestibulum. Euismod condimentum interdum suscipit maximus; tincidunt vulputate lobortis. Eu lectus bibendum in varius, velit parturient. Aelementum elit hendrerit mauris class turpis nunc. Sem nostra consequat tincidunt penatibus dapibus tempor dis. Tempor dolor bibendum; himenaeos luctus vivamus pharetra.</p>
+                <h2 className="aboutPage-subtitle">Profile</h2>
+                <p className="aboutPage-para">
+                    I am an Electronic Engineer with a strong understanding of Circuit Design, Hardware Design, 
+                    and Computer Aided Design gained through my degree at King’s College London and hands-on experience in academic and industrial settings. 
+                    During my final year at university, 
+                    I completed an individual research project designing, testing and building Variable Impedance Actuators (VIAs). 
+                </p>
+                <h2 className="aboutPage-subtitle">Industry Experience</h2>
+                <p className="aboutPage-para">
+                    I also have strong industry experience gained during my time at Lightricity Ltd in Oxford where I was in charge of designing a 
+                    circuit to allow for accurate light intensity measurements to be obtained from a solar cell. 
+                </p>
+                <h2 className="aboutPage-subtitle">Self-Taught</h2>
+                <p className="aboutPage-para">
+                    Beyond academics and industry, I have expanded my skills through self-directed learning and projects. 
+                    My passion for engineering, combined with hands-on experience, 
+                    enables me to tackle complex engineering challenges effectively.
+                </p>
             </div>
             
         </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,10 @@ export default function Home() {
             <div className="heroSection-imgParaCTA">
                 <div className="heroSection-imgPara">
                     <div className="heroSection-imgCTA">
-                        <p className="heroSection-headerPara">Lorem ipsum odor amet, consectetuer adipiscing elit. Adolor consequat litora dapibus inceptos sem. Cras curabitur tempor penatibus vulputate sociosqu nulla. Tempor at vivamus tortor sit ornare ac adipiscing. Penatibus sem ultricies primis convallis libero facilisis nulla dapibus molestie. Tincidunt dolor nunc lectus per ultricies, est sit. Atempor enim auctor adipiscing feugiat lectus accumsan in. Urna dui elit tellus libero rutrum quis. Convallis finibus metus diam at vehicula, lacus litora.</p>
+                        <p className="heroSection-headerPara">
+                            I am an Electronic Engineer with a strong skillset in circuit, hardware and computer aided design. 
+                            My time at Lightricity has allowed me valuable real-world experience.  
+                            </p>
                         <img className="heroSection-img mobileImg" src="\assets\imgs\Bruno\Bruno-Portrait_v01_.jpg" height="450px" width="450px"/>
                         <a href="mailto:brudebattista@gmail.com" target="_blank" className="heroSection-CTAButton">CONTACT</a>
                     </div>
